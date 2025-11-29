a97914744a919b8ba3c11ea838cb206889d9a054
Rafael Tomassewski <rafaeltomass@yahoo.com.br>
2025-11-29 15:09:55 -0300

    fix: registra controller de evolucoes

--- src/evolucoes/evolucoes.module.ts
+++ src/evolucoes/evolucoes.module.ts
@@ -1,13 +1,18 @@
 // src/evolucoes/evolucoes.module.ts
 import { Module } from '@nestjs/common';
 import { EvolucoesService } from './evolucoes.service';
-import { PrismaService } from '../prisma.service'; // 1. Importe
+import { PrismaService } from '../prisma.service';
+// 1. IMPORTANTE: Importe o Controller aqui
+import { EvolucoesController } from './evolucoes.controller'; 
 
 @Module({
+  // 2. IMPORTANTE: Registre o Controller nesta lista
+  controllers: [EvolucoesController], 
+  
   providers: [
     EvolucoesService,
-    PrismaService // 2. Adicione
+    PrismaService
   ],
-  exports: [EvolucoesService], // 3. Exporte o serviço
+  exports: [EvolucoesService],
 })
 export class EvolucoesModule {}
